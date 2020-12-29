5dcdd6f217f917e32a7f3e58239f0ede214eda60
cpt-jsph <nabagnachflorydy@gmail.com>
2020-12-29 22:06:18 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+document.getElementById("copyTag").addEventListener("click", runTest);
+
+function runTest() {
+  var getTag = document.getElementById("tag").value;
+  var splitTag = getTag.split("").join("؜");
+  $("input#tag").val(splitTag).select();
+  document.execCommand("copy");
+  $("input#tag").val(getTag);
+  document.getSelection().removeAllRanges();
+}
+
+/* eslint-env jquery */
