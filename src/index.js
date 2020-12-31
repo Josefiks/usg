c43ea7f4e008dd73c5c354cbd008af1f7ccdae80
cpt-jsph <nabagnachflorydy@gmail.com>
2020-12-31 11:27:42 +0100

    update sounds update sounds

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 document.getElementById("copyTag").addEventListener("click", copyTag);
-
+var $input = $("input#tag");
+$input.keyup(function (e) {
+  var max = 6;
+  if ($input.val().length > max) {
+    var audio = new Audio("src/sound/hit.mp3");
+    audio.volume = 0.1;
+    audio.play();
+    $input.val($input.val().substr(0, max));
+  }
+});
 function copyTag() {
   var getTag = document.getElementById("tag").value;
   var splitTag = getTag.split("").join("؜");
   $("input#tag").val(splitTag).select();
   document.execCommand("copy");
+
+  var audio = new Audio("src/sound/buttonclickrelease.ogg");
+  audio.volume = 0.3;
+  audio.play();
+
   $("input#tag").val(getTag);
   document.getSelection().removeAllRanges();
 }
